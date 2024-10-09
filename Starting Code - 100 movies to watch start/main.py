185dba67414774c3d5dbd437ca8e1a972d7d60c7
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-10-09 11:29:22 +0200

    finish movie list from website

--- Starting Code - 100 movies to watch start/main.py
+++ Starting Code - 100 movies to watch start/main.py
@@ -5,4 +5,18 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 
+response = requests.get(URL)
+website = BeautifulSoup(response.text, "html.parser")
+
+all_movies = website.find_all(name="h3", class_="title")
+
+movie_titles = [movie.getText() for movie in all_movies]
+#reverse list
+movies =movie_titles[::-1]
+# #or
+# for n in range(len(movie_titles) -1, -1, -1):
+#     print(movie_titles[n])
+with open("movies.txt", mode="w", encoding="utf-8") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
 
